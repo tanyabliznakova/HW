46af0c0cec0f0a9bf24b4289926c42d89eed26e1
tanyabliznakova <94783757+tanyabliznakova@users.noreply.github.com>
2021-11-28 11:36:57 +0200

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,4 @@
+w = int (input("Please enter your weight in kilograms:"))
+h = int (input("Please enter your height in meters:"))
+print ("Your body mass index is:",round(w/(h*h), 2))
+
---
+++ acad_to_astro_hours.py
@@ -0,0 +1,12 @@
+# Astronomical hour in minutes - x
+x = 60
+# Academical hour in minutes - y
+y = 40
+#PythonCourse Academical hours - a
+a = 64
+# Brakes - for each 3 academical hours there is a 15 minutes break
+z = (a/3) *15
+
+print ("The Python course in astronimical hours:", ((a*y) + z) /60  )
+
+

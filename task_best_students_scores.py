3b66bdd1150c21feeb228a1de0674f1ded19dceb
tanyabliznakova <94783757+tanyabliznakova@users.noreply.github.com>
2021-12-22 14:49:57 +0200

    Add files via upload

---
+++ task_best_students_scores.py
@@ -0,0 +1,20 @@
+students_score = {
+    'Ivan': 5.00,
+    'Alex': 3.50,
+    'Maria': 5.50,
+    'Georgy': 5.00
+}
+
+best_students_score = {}
+
+best_students_score = dict((k, v) for k, v in students_score.items() if v >= 4.00)
+for k,v in best_students_score.items():
+    print(f'{k} - {v}')
+
+
+
+
+
+
+
+
